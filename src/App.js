1bc2c93292498417b81b0a6835905dbc0d8bb0ce
Binamragupta <bgb221@nist.edu>
2023-08-10 01:34:17 +0530

    7th

--- src/App.js
+++ src/App.js
@@ -36,12 +36,12 @@ function App() {
     <>
     {/*  keep router at outside inside which is routes(switch in older version) inside which is route  where u keep the element of component based on the respective path
     '/'-represent home */}
-    <Router basename="/Authoreshare">
+    <Router basename="/">
     <Navbar title="Authoreshare" aboutText="about us" mode={mode} togglemode={togglemode}/>
     <Alert alert={alert}/>
     <div className="container my-4">
     <Routes>
-          <Route exact path="/Authoreshare" element={<TextForm heading="Enter a text to analyze below" mode={mode} showalert={showalert}/>}/>
+          <Route exact path="/" element={<TextForm heading="Enter a text to analyze below" mode={mode} showalert={showalert}/>}/>
           <Route exact path="/about" element={<About mode={mode}/>}/>
           {/* <TextForm heading="Enter a text to analyze below" mode={mode} showalert={showalert}/> */}
           
